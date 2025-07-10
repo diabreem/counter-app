837b95b1ba0a5e9a883e56c6ea8760704ca27d74
reem <diabreem7@gmail.com>
2025-07-10 21:56:42 +0300

    Added script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+function increment(){
+    count++;
+    updateDisplay();
+}
+function decrement(){
+    count--;
+    updateDisplay();
+}
+function reset(){
+    count=0;
+    updateDisplay();
+}
+function updateDisplay(){
+    document.getElementById("counter").innerText = count;
+}
